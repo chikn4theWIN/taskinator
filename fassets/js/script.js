8bcc613cafa67d4266a66a659778ff546fba4fbd
db <jacboysent@gmail.com>
2022-09-05 18:24:08 -0500

    added form submission

--- fassets/js/script.js
+++ fassets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+  event.preventDefault();
+
   var listItemE1 = document.createElement("li");
   listItemE1.className = "task-item";
   listItemE1.textContent = "This is a new task.";
   taskToDoE1.appendChild(listItemE1);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
