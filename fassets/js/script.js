bf11032a3a9fb2d530f1fed9905490e6f9d5fde3
db <jacboysent@gmail.com>
2022-09-05 18:57:00 -0500

    working form submit

--- fassets/js/script.js
+++ fassets/js/script.js
@@ -1,13 +1,25 @@
 var formEl = document.querySelector("#task-form");
-var taskToDoE1 = document.querySelector("#tasks-to-do");
+var taskToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-value']").value;
 
-  var listItemE1 = document.createElement("li");
-  listItemE1.className = "task-item";
-  listItemE1.textContent = "This is a new task.";
-  taskToDoE1.appendChild(listItemE1);
-}
+  //create a list item!
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // create a div to hold task info and add to list item.
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to the div.
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-value'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild (taskInfoEl);
+
+  // add entire list item to the list.
+  taskToDoEl.appendChild(listItemEl);
+};
 
 formEl.addEventListener("submit", createTaskHandler);
